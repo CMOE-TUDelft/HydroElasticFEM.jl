557cfc3c91cc4c643b473194c5ad485fbee116cd
Shagun Agarwal <shagun.1994@gmail.com>
2025-12-19 01:46:39 +0100

    Added initial notebook for single spring-mass damped system analysis using Julia code.

--- src/HydroElasticFEM.jl
+++ src/HydroElasticFEM.jl
@@ -2,6 +2,11 @@ module HydroElasticFEM
 
 const PKG_ROOT = normpath(joinpath(@__DIR__, ".."))  # because @__DIR__ here is src/
 
+include(joinpath(PKG_ROOT, "src", "Resonator", "Resonator.jl"))
+
+using .Resonator
+
+
 greet() = print("Hello World!")
 
 end # module HydroElasticFEM
