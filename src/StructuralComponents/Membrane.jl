10fc101c66fee6ebc00a0db1b0f821bf258b9e40
Shagun Agarwal <shagun.1994@gmail.com>
2026-01-28 17:15:51 +0100

    Re-organised the logs in notes

--- src/StructuralComponents/Membrane.jl
+++ src/StructuralComponents/Membrane.jl
@@ -28,7 +28,7 @@ struct Fixed <: MembraneBndType end
   # Derived quantities
   MTotal::Real # Total Mass per unit width
   ωn1::Real # Dry Analytical Natural frequency
-  
+
   function Membrane2D( L, m, T, τ, bndType::MembraneBndType )
     MTotal = m * L
     ωn1 = (π / L) * sqrt( T / m )
@@ -36,6 +36,11 @@ struct Fixed <: MembraneBndType end
   end
 
 end
+
+function Membrane2D(bndType::MembraneBndType)
+  Membrane2D( 0.0, 0.0, 0.0, 0.0, bndType )
+end
+
 # ----------------------End----------------------
 
 
@@ -52,10 +57,10 @@ function print_membrane_props( memb2D::Membrane2D, ρw::Real = 1025 )
   Tᵨ = memb2D.T / ρw
 
   @printf("\n[MSG] Membrane Properties:\n")
-  @printf("Density of water, ρw = %.2f kg/m3\n", ρw)
+  @printf("[VAL] Density of water, ρw = %.2f kg/m3\n", ρw)
   @printf("[VAL] Lm = %.4f m\n", memb2D.L)
   @printf("[VAL] m, mᵨ = %.4f kg/m2, %.4f m\n", memb2D.m, mᵨ)
-  @printf("[VAL] T, Tᵨ = %.4f N/m, %.4f m3/s2\n", memb2D.T, Tᵨ)
+  @printf("[VAL] T, Tᵨ = %.4f MN/m, %.4f m3/s2\n", memb2D.T/1e6, Tᵨ)
   @printf("[VAL] τ = %.4f \n", memb2D.τ)
   @printf("[VAL] memBndType = %s \n", string(memb2D.bndType))
   @printf("[VAL] MTotal = %.4f kg/m \n", memb2D.MTotal)
